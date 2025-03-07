d5d04d58961dc64b1b035ae2deaf09a6436b2086
Nicolas Casser <nicolascasser4@gmail.com>
2025-03-07 16:53:00 -0300

    Exercicio 1 finalizado

---
+++ 1. Contagem de 1 a 10.js
@@ -0,0 +1,6 @@
+//Escreva um programa que exiba os números de 1 a 10, um em cada linha, usando a estrutura de repetição FOR.
+var prompt = require('prompt-sync')();
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
